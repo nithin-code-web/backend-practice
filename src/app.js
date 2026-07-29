65375a98521e7fcf6b2ffa837df72f9892280aaf
Nithin Budime <nithinbudime@gmail.com>
2026-07-29 10:10:10 +0000

    implement custom logger middleware

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 import express from "express";
 import userRoutes from "./routes/user.routes.js";
+import logger from "./middlewares/logger.js";
 
 const app = express();
 app.use(express.json());
+app.use(logger)
 
 
 app.get('/',(req,res) => {
