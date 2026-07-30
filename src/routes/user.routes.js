0ad8ce55fc75396a68295a414fdfacc452d6828b
Nithin Budime <nithinbudime@gmail.com>
2026-07-30 06:32:04 +0000

    day-6: implement full REST API (PUT, PATCH, DELETE) with validateBody middleware

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,11 +1,13 @@
 import { Router } from 'express'
 const router = Router();
-import { getAllUsers,createUser,getUserById,searchUsers } from '../controllers/user.controller.js'
+import { getAllUsers,createUser,getUserById,searchUsers,deleteUser,replaceUser,updateUser } from '../controllers/user.controller.js'
 import { validateBody } from '../middlewares/validateBody.js'
 
 router.post('/users',validateBody(['name','email']),createUser)
 router.get('/users',getAllUsers)
 router.get('/users/search',searchUsers)
 router.get('/users/:id',getUserById)
-
+router.delete('/users/:id',deleteUser)
+router.put('/users/:id',validateBody(['name','email']),replaceUser)
+router.patch('/users/:id',updateUser)
 export default router;
